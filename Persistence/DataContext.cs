dbdb75c4f5bed78411453a8ddd1ce41718caa332
Bettina Pattenson <bettina.pattenson@baycare.org>
2021-09-20 13:22:14 -0400

    Checking in

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+        public object DataBase { get; set; }
+    }
+}
+
