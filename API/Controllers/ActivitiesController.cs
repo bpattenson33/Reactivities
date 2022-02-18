1aceb24ab80582f482ee4b54fb3cbf09b7fed506
bpattenson33 <90928245+bpattenson33@users.noreply.github.com>
2022-02-18 11:03:45 -0500

    Add files via upload

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+//using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [AllowAnonymous]
+    public class ActivitiesController : BaseApiController
+    {     
+
+        [HttpGet]
+        public async Task<IActionResult> GetActivities()
+        {
+            return HandleResult(await Mediator.Send(new Application.Activities.List.Query()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetActivity(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query{Id = id}));           
+        }
+
+         [HttpPost]
+         public async Task<IActionResult> CreateActivity(Activity activity)
+         {
+             return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
+         }
+
+         [HttpPut("{id}")]
+         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+         {
+             activity.Id = id;
+             return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity } ));
+         }
+
+         [HttpDelete("{id}")]
+         public async Task<IActionResult> DeleteActivity(Guid id)
+         {
+             return HandleResult(await Mediator.Send(new Delete.Command { Id = id}));
+         }
+    }
+}
